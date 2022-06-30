f83ecb01bcbd16f1c2cc910f981f2200b7917ba3
Seungwoo Son <sson68x@gmail.com>
2022-06-29 23:35:04 -0700

    initial commit

---
+++ src/driver/handleDriver.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const eventPool = require('../eventPool');
+const { order } = require('../chance');
+
+eventPool.on('PICKEDUP', driverPickedUp);
+eventPool.on('IN_TRANSIT', driverInTransit);
+eventPool.on('DELIVERED', driverDelivered);
+
+const payload = order;
+
+function driverPickedUp() {
+    console.log(`Package PICKED UP. Order# ${payload.orderID}`);
+    eventPool.emit('IN_TRANSIT', payload);
+};
+
+function driverInTransit() {
+    console.log(`Package In TRANSIT. Order# ${payload.orderID}`);
+    // eventPool.emit('DELIVERED', payload);
+};
+
+function driverDelivered() {
+    console.log(`Package DELIVERED. Order# ${payload.orderID}`);
+};
+
+
+module.exports = {
+    driverPickedUp,
+    driverInTransit,
+    driverDelivered,
+};
+
